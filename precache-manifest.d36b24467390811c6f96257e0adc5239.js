4e48cee415f05b76f62e552bd651e33d685d287c
Jennifer W <jenniferwalason@gmail.com>
2020-09-15 17:08:31 -0400

    Updates

---
+++ precache-manifest.d36b24467390811c6f96257e0adc5239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821dbff3cca0010e2c727897527e1aad",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6dcad71a76ec2b15b589",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "417f2a840f9a33990270",
+    "url": "/react-portfolio/static/css/main.082329f1.chunk.css"
+  },
+  {
+    "revision": "6dcad71a76ec2b15b589",
+    "url": "/react-portfolio/static/js/2.b0f9e80a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.b0f9e80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417f2a840f9a33990270",
+    "url": "/react-portfolio/static/js/main.f3ece22b.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "a100dde70a04e7c516669fcfc3b9d6e9",
+    "url": "/react-portfolio/static/media/jlw-resume.a100dde7.pdf"
+  }
+]);
